9c4ae2cb2843ec7699a0be420d21b73e2ea1d91c
Tanmay Arora <67386710+tanmayarora11@users.noreply.github.com>
2021-01-07 00:41:06 +0530

    projects,education and experience updated

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer", "WebDeveloper", "Coder","ML Enthusiast","AndriodDeveloper"],
+        strings: ["Engineer", "WebDeveloper", "Competitive-Programmer","ML Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineer", "WebDeveloper", "Coder","ML Enthusiast","AndriodDeveloper"],
+        strings: ["Engineer", "WebDeveloper", "Competitive-Programmer","ML Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
